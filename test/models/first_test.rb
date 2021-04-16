c41f0c0809d1ce63c35786d303ca4c8a2f23da0b
Alisson Rangel <alisson.rangel@hotmail.com>
2021-04-16 14:48:50 -0300

    commit inicial com a criacao do home_controler route e show.html.erb, criação de um arquivo de test: first_test.rb

---
+++ test/models/first_test.rb
@@ -0,0 +1,9 @@
+require "test_helper"
+
+class FirstTest < ActiveSupport::TestCase
+    test "Esse é o meu primeiro teste" do
+        variavel = "Meu nome é Alisson"
+
+        assert_equal "Meu nome é Alisson", variavel
+    end
+end
