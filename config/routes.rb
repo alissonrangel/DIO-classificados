2914b2fe4059a863c912e4b0c8dfd09062b66240
Alisson Rangel <alisson.rangel@hotmail.com>
2021-04-17 13:37:23 -0300

    parte 18 continuando a i18n,rotas através de resources, t(".success").

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#show"
-  get "/users/new", to: "users#new"
-  post "/users", to: "users#create"
+  
+  # get "/users/new", to: "users#new"
+  # post "/users", to: "users#create"
 
-  get "/sessions/new", to: "sessions#new"
+  resources :users, only: [:new, :create]
+  '''
+  get "users/:id/edit", to: "users#edit"
+  patch "/users/:id", to: "users#update"
+  put "/users/:id", to: "users#update"
+  delete "/users/:id", to: "users#destroy"
+  get "/users", to: "users#index"
+  get "users/:id", to: "users#show"
+  '''
+
+  # get "/sessions/new", to: "sessions#new"
+
+  resources :sessions, only: [:new]
 end
