12b77b6da6fb9bf6be0bb8c6258cba770b0b715f
Alisson Rangel <alisson.rangel@hotmail.com>
2021-04-18 14:19:39 -0300

    parte 25, mail preview test, alteração no html e text da mensagem de email

--- test/mailers/previews/notification_mailer_preview.rb
+++ test/mailers/previews/notification_mailer_preview.rb
@@ -3,7 +3,7 @@ class NotificationMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/welcome
   def welcome
-    NotificationMailer.welcome
+    NotificationMailer.welcome("Alisson","alisson.rangel@hotmail.com")
   end
 
 end
